ee039c1cb103a7551f9da82a05a49d185c23d97e
Per Hansen <me@perhp.com>
2024-12-27 17:10:42 +0100

    chore: prettier

--- index.js
+++ index.js
@@ -1,74 +1,99 @@
 const spawn = require("child_process").spawn;
 
 function getCPUTemperature(callback) {
-    const regex = /temp=([^'C]+)/;
-    const cmd = spawn("/usr/bin/vcgencmd", ["measure_temp"]);
+  const regex = /temp=([^'C]+)/;
+  const cmd = spawn("/usr/bin/vcgencmd", ["measure_temp"]);
 
-    cmd.stdout.once("data", (data) => {
-        const match = data.toString("utf8").match(regex);
-        callback(match ? parseFloat(match[1]) : null);
-    });
-    
-    cmd.stderr.once("data", () => {
-        callback(null);
-    });
+  cmd.stdout.once("data", (data) => {
+    const match = data.toString("utf8").match(regex);
+    callback(match ? parseFloat(match[1]) : null);
+  });
+
+  cmd.stderr.once("data", () => {
+    callback(null);
+  });
 }
 
 function getMemoryUsage(callback) {
-    const cmd = spawn("free");
+  const cmd = spawn("free");
 
-    cmd.stdout.once("data", (data) => {
-        const usages = data.toString("utf8").split("\n")[1].replace(/( +)/g, " ").split(" ");
-        const [total, used, free, shared, buffCache, available] = usages.splice(1).map(usage => parseFloat(usage))
+  cmd.stdout.once("data", (data) => {
+    const usages = data
+      .toString("utf8")
+      .split("\n")[1]
+      .replace(/( +)/g, " ")
+      .split(" ");
+    const [total, used, free, shared, buffCache, available] = usages
+      .splice(1)
+      .map((usage) => parseFloat(usage));
 
-        callback({ total, used, free, shared, buffCache, available });
-    });
-    
-    cmd.stderr.once("data", () => {
-        callback(null);
-    });
+    callback({ total, used, free, shared, buffCache, available });
+  });
+
+  cmd.stderr.once("data", () => {
+    callback(null);
+  });
 }
 
 function getDiskUsage(callback) {
-    const cmd = spawn("df");
+  const cmd = spawn("df");
 
-    cmd.stdout.once("data", (data) => {
-        const usages = data.toString("utf8").split("\n").filter(Boolean).splice(1).map(usageText => {
-            const [filesystem, oneKBlocks, used, available, usePercentage, mountedOn] = usageText.replace(/( +)/g, " ").split(" ");
-            return {
-                filesystem, 
-                oneKBlocks: parseFloat(oneKBlocks), 
-                used: parseFloat(used), 
-                available: parseFloat(available), 
-                usePercentage: parseFloat(usePercentage.replace("%", "")), 
-                mountedOn
-            }
-        })
-        callback(usages);
-    });
-    
-    cmd.stderr.once("data", () => {
-        callback(null);
-    });
+  cmd.stdout.once("data", (data) => {
+    const usages = data
+      .toString("utf8")
+      .split("\n")
+      .filter(Boolean)
+      .splice(1)
+      .map((usageText) => {
+        const [
+          filesystem,
+          oneKBlocks,
+          used,
+          available,
+          usePercentage,
+          mountedOn,
+        ] = usageText.replace(/( +)/g, " ").split(" ");
+        return {
+          filesystem,
+          oneKBlocks: parseFloat(oneKBlocks),
+          used: parseFloat(used),
+          available: parseFloat(available),
+          usePercentage: parseFloat(usePercentage.replace("%", "")),
+          mountedOn,
+        };
+      });
+    callback(usages);
+  });
+
+  cmd.stderr.once("data", () => {
+    callback(null);
+  });
 }
 
 function asynchronize(candidate, errorMessage) {
-    return () => new Promise((resolve, reject) => {
-        candidate(value => {
-            if (value === null) {
-                reject(new Error(errorMessage));
-            }
+  return () =>
+    new Promise((resolve, reject) => {
+      candidate((value) => {
+        if (value === null) {
+          reject(new Error(errorMessage));
+        }
 
-            resolve(value);
-        })
-    })
+        resolve(value);
+      });
+    });
 }
 
 module.exports = {
+  getCPUTemperature,
+  getCPUTemperatureAsync: asynchronize(
     getCPUTemperature,
-    getCPUTemperatureAsync: asynchronize(getCPUTemperature, "Failed to read CPU temperature"),
+    "Failed to read CPU temperature",
+  ),
+  getMemoryUsage,
+  getMemoryUsageAsync: asynchronize(
     getMemoryUsage,
-    getMemoryUsageAsync: asynchronize(getMemoryUsage, "Failed to read memory usage"),
-    getDiskUsage,
-    getDiskUsageAsync: asynchronize(getDiskUsage, "Failed to read disk usage"),
-}
+    "Failed to read memory usage",
+  ),
+  getDiskUsage,
+  getDiskUsageAsync: asynchronize(getDiskUsage, "Failed to read disk usage"),
+};
