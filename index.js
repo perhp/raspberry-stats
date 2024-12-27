3ef51f4d82085b0f4e1458db80ef6fdb29e336ee
Per Hansen <me@perhp.com>
2024-12-27 16:40:02 +0100

    feat: get memory usage

--- index.js
+++ index.js
@@ -26,7 +26,36 @@ function getCPUTemperatureAsync() {
     });
 }
 
+function getMemoryUsage(callback) {
+    const cmd = spawn("free");
+
+    cmd.stdout.once("data", (data) => {
+        const usages = data.toString("utf8").split("\n")[1].replace(/( +)/g, " ").split(" ");
+        const [total, used, free, shared, buffCache, available] = usages.splice(1).map(usage => parseFloat(usage))
+
+        callback({ total, used, free, shared, buffCache, available });
+    });
+    
+    cmd.stderr.once("data", () => {
+        callback(null);
+    });
+}
+
+function getMemoryUsageAsync() {
+    return new Promise((resolve, reject) => {
+        getMemoryUsage((usage) => {
+            if (usage === null) {
+                reject(new Error("Failed to read memory usage"))
+            }
+
+            resolve(usage);
+        });
+    });
+}
+
 module.exports = {
     getCPUTemperature,
     getCPUTemperatureAsync,
+    getMemoryUsage,
+    getMemoryUsageAsync,
 }
