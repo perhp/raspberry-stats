b6991d2aa4f6f40156145a66180242cac0bca7d7
Per Hansen <me@perhp.com>
2024-12-27 16:56:29 +0100

    feat: get disk usage

--- index.js
+++ index.js
@@ -53,9 +53,46 @@ function getMemoryUsageAsync() {
     });
 }
 
+function getDiskUsage(callback) {
+    const cmd = spawn("df");
+
+    cmd.stdout.once("data", (data) => {
+        const usages = data.toString("utf8").split("\n").filter(Boolean).splice(1).map(usageText => {
+            const [filesystem, oneKBlocks, used, available, usePercentage, mountedOn] = usageText.replace(/( +)/g, " ").split(" ");
+            return {
+                filesystem, 
+                oneKBlocks: parseFloat(oneKBlocks), 
+                used: parseFloat(used), 
+                available: parseFloat(available), 
+                usePercentage: parseFloat(usePercentage.replace("%", "")), 
+                mountedOn
+            }
+        })
+        callback(usages);
+    });
+    
+    cmd.stderr.once("data", () => {
+        callback(null);
+    });
+}
+
+function getDiskUsageAsync() {
+    return new Promise((resolve, reject) => {
+        getDiskUsage((usage) => {
+            if (usage === null) {
+                reject(new Error("Failed to read disk usage"))
+            }
+
+            resolve(usage);
+        });
+    });
+}
+
 module.exports = {
     getCPUTemperature,
     getCPUTemperatureAsync,
     getMemoryUsage,
     getMemoryUsageAsync,
+    getDiskUsage,
+    getDiskUsageAsync,
 }
